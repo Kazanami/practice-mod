b27c4f3e922d34e7187f2d28702e9d3f575f5088
Kazanami <windowslen@users.noreply.github.com>
2019-04-17 19:08:14 +0900

    交換アイテム追加・更新

--- src/main/java/kazanami/kazanami_shop/main/shop_kazanami.java
+++ src/main/java/kazanami/kazanami_shop/main/shop_kazanami.java
@@ -1,6 +1,7 @@
 package kazanami.kazanami_shop.main;
 
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemNameTag;
@@ -37,10 +38,28 @@ public class shop_kazanami {
     //Minecraft Item
     public static Item exp_bottle;
 
-    //Shincolle Items
-    public static Item Onnen_Item;
-    public static Block Onnen_Block;
-    public static Block Onnen_Heavy_Block;
+    //Chinjuhu-mod Items
+    //public static String GET_ITEM_MOD = "chinjuhumod";
+    public static Item Seed_corn;
+    public static Item Seed_hakusai;
+    public static Item Seed_onion;
+    public static Item Seed_spinach;
+    public static Item Seed_cabbage;
+    public static Item Seed_rice;
+    public static Item Seed_soy;
+    public static Item Seed_tomato;
+    /*
+    アイテムID郡
+    chinjufumod
+	    item_seeds_corn とうもろこし
+	    item_seeds_hakusai 白菜
+	    item_seeds_onion 玉ねぎ
+	    item_seeds_spinach ほうれん草
+	    item_seeds_cabbage キャベツ
+	    item_seeds_rice 種籾
+	    item_seeds_soy 大豆
+	    item_seeds_tomato トマト
+     */
 
 
     @Mod.EventHandler
@@ -74,12 +93,43 @@ public class shop_kazanami {
             }
         }*/
         //GameRegistry.addShapelessRecipe(new ItemStack(Items.EXPERIENCE_BOTTLE, 1), new ItemStack(Items.IRON_INGOT, 3));
-        GameRegistry.addRecipe(new ItemStack(Items.EXPERIENCE_BOTTLE),
+        if (Loader.isModLoaded("chinjufumod")){
+            try{
+                /*アイテム変数追加処理*/
+                this.Seed_corn = GameRegistry.findItem("chinjufumod", "item_seeds_corn");
+                this.Seed_cabbage = GameRegistry.findItem("chinjufumod", "item_seeds_cabage");
+                this.Seed_hakusai = GameRegistry.findItem("chinjufumod", "item_seeds_hakusai");
+                this.Seed_onion = GameRegistry.findItem("chinjufumod", "item_seeds_onion");
+                this.Seed_rice = GameRegistry.findItem("chinjufumod", "item_seeds_rice");
+                this.Seed_soy = GameRegistry.findItem("chinjufumod", "item_seeds_soy");
+                this.Seed_spinach = GameRegistry.findItem("chinjufumod", "item_seeds_spinach");
+                this.Seed_tomato = GameRegistry.findItem("chinjufumod", "item_seeds_tomato");
+
+                /*カスタムレシピ追加処理*/
+                GameRegistry.addRecipe(new ItemStack(this.Seed_corn, 64),
+                        "SSS",
+                        "   ",
+                        "   ",
+                        'S', new ItemStack(Items.BEETROOT_SEEDS, 1)
+                        );
+
+            }catch (Throwable t){
+                logger.warn("Faild to get Item of Chinjufu-mod");
+            }finally{
+                logger.debug("Loaded Recipe");
+            }
+        }
+
+
+        /*最後に読み込ませる 鉄インゴット3から経験値瓶3*/
+        GameRegistry.addRecipe(new ItemStack(Items.EXPERIENCE_BOTTLE, 3),
                 " I ",
-                "I I",
+                "III",
                 "   ",
                 'I', new ItemStack(Items.IRON_INGOT)
         );
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.EXPERIENCE_BOTTLE, 20), new ItemStack(Blocks.IRON_BLOCK, 2));
+
         proxy.postInit(event);
     }
 
