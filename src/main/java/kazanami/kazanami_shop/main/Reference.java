4e8120863b12bf283b0eb8da27a253a770d276d7
Kazanami <windowslen@users.noreply.github.com>
2019-04-17 22:16:32 +0900

    バージョンアップデート

--- src/main/java/kazanami/kazanami_shop/main/Reference.java
+++ src/main/java/kazanami/kazanami_shop/main/Reference.java
@@ -5,7 +5,7 @@ package kazanami.kazanami_shop.main;
 public class Reference {
     public static final String MOD_ID = "kazanamishop";
     public static final String MOD_NAME = "Kazanami Shop";
-    public static final String MOD_VERSION = "0.0.1 alpha";
+    public static final String MOD_VERSION = "1.0.1 alpha";
     public static final String CLIENT_PROXY_CLASS = "kazanami.kazanami_shop.proxy.ClientProxy";
     public static final String COMMON_PROXY_CLASS = "kazanami.kazanami_shop.proxy.CommonProxy";
 }
